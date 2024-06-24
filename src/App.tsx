d43a07d05d6e1c2b29d263f023ceaf4485013321
Marcos Dias da Conceição <ti.marcosdias@gmail.com>
2024-06-24 12:40:31 -0300

    feat: update content and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,61 @@
-import avatar from './assets/avatar.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import avatar from './assets/avatar.png';
 import AppTemplate from './components/templates/AppTemplate/AppTemplate';
+import { faDev, faGithub, faGitlab, faLinkedin, faMedium, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const App: React.FunctionComponent = () => {
   return (
     <AppTemplate>
-      <div className="w-full max-w-xl my-0 mx-auto p-2 text-center">
-        <div className="flex justify-center p-3">
-          <a href="https://www.linkedin.com/in/marcos-dias-67177931/" target="_blank" rel="noreferrer">
-            <img src={avatar} className="rounded-full w-1/2 mx-auto" alt="Avatar" />
-          </a>
+      <div className="w-full max-w-xl my-0 mx-auto p-2 text-center md:text-left grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
+        <div id="avatar-container" className="flex justify-center col-span-1">
+          <img src={avatar} className="rounded-full w-1/2 md:w-max mx-auto" alt="Avatar" />
         </div>
-        <h1>Marcos Dias</h1>
-        <h2>Software Engineer</h2>
+        <div id="info-container" className="col-span-2 h-fit">
+          <div className="mb-4">
+            <h1 className="font-bold">Marcos Dias</h1>
+            <h2>Software Engineer</h2>
+          </div>
+          <div className="flex justify-center md:justify-start gap-4">
+            <a
+              href="https://www.linkedin.com/in/marcos-dias-67177931/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-inherit transition"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="xl" />
+            </a>
+            <a href="https://x.com/marcosdiasdev" target="_blank" rel="noreferrer" className="text-inherit transition">
+              <FontAwesomeIcon icon={faXTwitter} size="xl" />
+            </a>
+            <a
+              href="https://github.com/marcosdiasdev"
+              target="_blank"
+              rel="noreferrer"
+              className="text-inherit transition"
+            >
+              <FontAwesomeIcon icon={faGithub} size="xl" />
+            </a>
+            <a
+              href="https://gitlab.com/marcos.dias3"
+              target="_blank"
+              rel="noreferrer"
+              className="text-inherit transition"
+            >
+              <FontAwesomeIcon icon={faGitlab} size="xl" />
+            </a>
+            <a href="https://dev.to/marcosdiasdev" target="_blank" rel="noreferrer" className="text-inherit transition">
+              <FontAwesomeIcon icon={faDev} size="xl" />
+            </a>
+            <a
+              href="https://medium.com/@marcosdiasdev"
+              target="_blank"
+              rel="noreferrer"
+              className="text-inherit transition"
+            >
+              <FontAwesomeIcon icon={faMedium} size="xl" />
+            </a>
+          </div>
+        </div>
       </div>
     </AppTemplate>
   );
--- src/components/templates/AppTemplate/AppTemplate.tsx
+++ src/components/templates/AppTemplate/AppTemplate.tsx
@@ -1,7 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
-import { useCallback, useState } from 'react';
-
 export interface Props {
   className?: string;
   children: React.ReactNode;
@@ -9,16 +5,9 @@ export interface Props {
 
 // eslint-disable-next-line react/prop-types
 const AppTemplate: React.FunctionComponent<Props> = ({ children }) => {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light');
-  const toggleTheme = useCallback(() => setTheme((t) => (t === 'light' ? 'dark' : 'light')), [setTheme]);
-
   return (
     <div className="w-full">
-      <header className="w-full flex justify-end fixed top-0 left-0">
-        <button className="bg-transparent hover:border-transparent focus:outline-transparent" onClick={toggleTheme}>
-          <FontAwesomeIcon icon={theme === 'light' ? faSun : faMoon} size="2xl" />
-        </button>
-      </header>
+      <header className="w-full flex justify-end fixed top-0 left-0" />
       {children}
     </div>
   );
